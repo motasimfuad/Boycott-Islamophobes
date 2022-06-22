e1c1f3253107884ce6487aae0b12c7683c19539d
motasimfuad <motasimfuad0@gmail.com>
2022-06-22 08:19:58 +0600

    strings added

--- lib/core/constants/strings.dart
+++ lib/core/constants/strings.dart
@@ -1,1 +1,7 @@
-
+// assets
+class Images {
+  static const String countryIcon = 'assets/icons/country.png';
+  static const String productIcon = 'assets/icons/product.png';
+  // static const String companyIcon = 'assets/icons/company.png';
+  static const String categoryIcon = 'assets/icons/category.png';
+}
