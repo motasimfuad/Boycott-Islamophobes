cacf6a74fbfecf263146bc061685cb3d8dfb513d
motasimfuad <motasimfuad0@gmail.com>
2022-06-27 23:32:16 +0600

    di updated for country

--- lib/dependency_injection.dart
+++ lib/dependency_injection.dart
@@ -4,6 +4,12 @@ import 'package:boycott_islamophobes/features/category/domain/repositories/categ
 import 'package:boycott_islamophobes/features/category/domain/usecases/get_all_categories_usecase.dart';
 import 'package:boycott_islamophobes/features/category/domain/usecases/get_category_usecase.dart';
 import 'package:boycott_islamophobes/features/category/presentation/bloc/category_bloc.dart';
+import 'package:boycott_islamophobes/features/country/data/datasources/country_remote_data_source.dart';
+import 'package:boycott_islamophobes/features/country/data/repositories/country_repository_impl.dart';
+import 'package:boycott_islamophobes/features/country/domain/repositories/country_repository.dart';
+import 'package:boycott_islamophobes/features/country/domain/usecases/get_all_countries_usecase.dart';
+import 'package:boycott_islamophobes/features/country/domain/usecases/get_country_usecase.dart';
+import 'package:boycott_islamophobes/features/country/presentation/bloc/country_bloc.dart';
 import 'package:boycott_islamophobes/features/product/data/datasources/product_remote_data_source.dart';
 import 'package:boycott_islamophobes/features/product/data/repositories/product_repository_impl.dart';
 import 'package:boycott_islamophobes/features/product/domain/repositories/product_repository.dart';
@@ -35,19 +41,32 @@ Future<void> init() async {
     ),
   );
 
+  getIt.registerFactory(
+    () => CountryBloc(
+      getAllCountries: getIt(),
+      getCountry: getIt(),
+    ),
+  );
+
   // usecases
+  // --- products
   getIt.registerLazySingleton(() => GetAllProductsUsecase(getIt()));
   getIt.registerLazySingleton(() => GetProductUsecase(getIt()));
   getIt.registerLazySingleton(() => GetFilteredProductsUsecase(getIt()));
-
+  // --- categories
   getIt.registerLazySingleton(() => GetAllCategoriesUsecase(getIt()));
   getIt.registerLazySingleton(() => GetCategoryUsecase(getIt()));
+  // --- categories
+  getIt.registerLazySingleton(() => GetAllCountriesUsecase(getIt()));
+  getIt.registerLazySingleton(() => GetCountryUsecase(getIt()));
 
   // repositories
   getIt.registerLazySingleton<ProductRepository>(
       () => ProductRepositoryImpl(getIt()));
   getIt.registerLazySingleton<CategoryRepository>(
       () => CategoryRepositoryImpl(getIt()));
+  getIt.registerLazySingleton<CountryRepository>(
+      () => CountryRepositoryImpl(getIt()));
 
   //! core
   //! data
@@ -56,6 +75,8 @@ Future<void> init() async {
       () => ProductRemoteDataSourceImpl(firestore: getIt()));
   getIt.registerLazySingleton<CategoryRemoteDataSource>(
       () => CategoryRemoteDataSourceImpl(getIt()));
+  getIt.registerLazySingleton<CountryRemoteDataSource>(
+      () => CountryRemoteDataSourceImpl(getIt()));
 
   //! external
   getIt.registerLazySingleton(() => FirebaseFirestore.instance);
