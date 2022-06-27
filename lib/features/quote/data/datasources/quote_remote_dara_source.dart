78d5936c3ff088c60b8c920089c720244c8ecffe
motasimfuad <motasimfuad0@gmail.com>
2022-06-28 03:54:42 +0600

    quote dara source added

---
+++ lib/features/quote/data/datasources/quote_remote_dara_source.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../models/quote_model.dart';
+
+abstract class QuoteRemoteDataSource {
+  Future<List<QuoteModel>> getAllQuotes();
+}
+
+class QuoteRemoteDataSourceImpl implements QuoteRemoteDataSource {
+  final FirebaseFirestore firestore;
+  QuoteRemoteDataSourceImpl(this.firestore);
+
+  @override
+  Future<List<QuoteModel>> getAllQuotes() async {
+    final quoteCollection = await firestore.collection('quotes').get();
+    List<QuoteModel> quoteList =
+        quoteCollection.docs.map((e) => QuoteModel.fromMap(e.data())).toList();
+    return quoteList;
+  }
+}
