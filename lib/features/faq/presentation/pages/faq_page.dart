51b8ee2665a9f0c088bb9a9aa13bc3dd5b5971eb
motasimfuad <motasimfuad0@gmail.com>
2022-06-29 20:11:22 +0600

    dynamic faqs impplemented

--- lib/features/faq/presentation/pages/faq_page.dart
+++ lib/features/faq/presentation/pages/faq_page.dart
@@ -1,14 +1,32 @@
 import 'package:accordion/accordion.dart';
 import 'package:accordion/controllers.dart';
 import 'package:boycott_islamophobes/core/widgets/k_appbar.dart';
+import 'package:boycott_islamophobes/core/widgets/k_card.dart';
+import 'package:boycott_islamophobes/core/widgets/k_shimmer.dart';
+import 'package:boycott_islamophobes/features/faq/domain/entities/faq_entity.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../core/constants/colors.dart';
+import '../bloc/faq_bloc.dart';
 import '../widgets/faq_header.dart';
 
-class FaqPage extends StatelessWidget {
+class FaqPage extends StatefulWidget {
   const FaqPage({Key? key}) : super(key: key);
+
+  @override
+  State<FaqPage> createState() => _FaqPageState();
+}
+
+class _FaqPageState extends State<FaqPage> {
+  List<FaqEntity> faqs = [];
+
+  @override
+  void initState() {
+    context.read<FaqBloc>().add(GetAllFaqsEvent());
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,24 +48,33 @@ class FaqPage extends StatelessWidget {
               ),
               child: const FaqHeader(),
             ),
-            Accordion(
-              paddingListHorizontal: 20.w,
-              paddingListBottom: 80.h,
-              maxOpenSections: 1,
-              headerPadding: EdgeInsets.symmetric(
-                horizontal: 18.w,
-                vertical: 15.h,
-              ),
-              headerBorderRadius: 20.w,
-              disableScrolling: true,
-              children: List.generate(
-                15,
-                (index) => _buildAccordionSection(
-                  question:
-                      'Instance "ListController" with tag "279662130" has been initial adfiz',
-                  answer: 'answer 1',
-                ),
-              ),
+            BlocBuilder<FaqBloc, FaqState>(
+              builder: (context, state) {
+                if (state is FaqListLoading) {
+                  return const FaqLoadingList();
+                }
+                if (state is FaqListLoaded) {
+                  faqs = state.faqs;
+                }
+                return Accordion(
+                  paddingListHorizontal: 20.w,
+                  paddingListBottom: 80.h,
+                  maxOpenSections: 1,
+                  headerPadding: EdgeInsets.symmetric(
+                    horizontal: 18.w,
+                    vertical: 15.h,
+                  ),
+                  scrollIntoViewOfItems: ScrollIntoViewOfItems.fast,
+                  headerBorderRadius: 20.w,
+                  disableScrolling: true,
+                  children: faqs.map((faq) {
+                    return _buildAccordionSection(
+                      question: faq.question,
+                      answer: faq.answer,
+                    );
+                  }).toList(),
+                );
+              },
             ),
           ],
         ),
@@ -80,6 +107,38 @@ class FaqPage extends StatelessWidget {
       contentVerticalPadding: 15.w,
       paddingBetweenOpenSections: 10.h,
       paddingBetweenClosedSections: 10.h,
+    );
+  }
+}
+
+class FaqLoadingList extends StatelessWidget {
+  const FaqLoadingList({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.only(top: 20.h),
+      shrinkWrap: true,
+      primary: false,
+      itemCount: 3,
+      itemBuilder: (BuildContext context, int index) {
+        return KShimmer(
+          child: Padding(
+            padding: EdgeInsets.only(
+              left: 20.w,
+              right: 20.w,
+              bottom: 15.h,
+            ),
+            child: KCard(
+              hasShadow: false,
+              height: 60.h,
+              radius: 20.r,
+            ),
+          ),
+        );
+      },
     );
   }
 }
